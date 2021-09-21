4204b09fa8931fdcb46974c1325c878efa718602
funtalex <45655288+funtalex@users.noreply.github.com>
2021-09-22 01:33:07 +0300

    Add files via upload

---
+++ my_sort.h
@@ -0,0 +1,15 @@
+#ifndef my_sort
+#define my_sort
+
+
+struct line;
+
+int cmp_str_start(struct line, struct line);
+
+int cmp_str_end(struct line, struct line);
+
+void my_sort(struct line**, int, int, int);
+
+
+
+#endif my_sort
